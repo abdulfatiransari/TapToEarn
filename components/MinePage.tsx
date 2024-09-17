70454a2b24378458a60946ea86ceaf99d770e53a
HamzaaliUddin <hamzaalikhan20611@gmail.com>
2024-09-17 16:51:35 +0500

    add coming soon text on mine and remove resuable profit concept

--- components/MinePage.tsx
+++ components/MinePage.tsx
@@ -4,12 +4,12 @@ import UpgradeProfit from "./UpgradeProfit";
 
 export default function MinePage() {
     return (
-        <div className="bg-[#341355] pt-[5px] rounded-t-[36px] h-full">
+        <div className="bg-[#341355] pt-[5px] rounded-t-[36px] ">
             <div style={{
                 background: "radial-gradient(circle, rgba(90, 0, 150, 0.8) 40%, rgba(0, 0, 0, 1) 100%)",
+                height: "100vh",
             }} className=" rounded-t-[36px] pt-1 px-2 flex flex-col gap-4">
                 <div className="px-2">
-                    <Score />
                     <UpgradeProfit />
                 </div>
             </div>
--- components/UpgradeProfit.tsx
+++ components/UpgradeProfit.tsx
@@ -2,229 +2,11 @@ import Image from "next/image";
 import React, { useState } from "react";
 import BottomModal from "./BottomModal";
 import { RxCross2 } from "react-icons/rx";
+import { degensData, flippersData, holdersData, Speclals, tabs, whalesData } from "@/constant/MinTab";
 
 export default function UpgradeProfit() {
     const [tabActive, setTabActive] = useState<string>("Flippers");
     const [isOpen, setOpen] = useState<boolean>(false);
-    const tabs = [
-        {
-            title: "Flippers",
-        },
-        {
-            title: "Degens",
-        },
-        {
-            title: "Holders",
-        },
-        {
-            title: "Whales",
-        },
-    ];
-
-    const flippersData = [
-        {
-            img: "",
-            title: "Airdroplandia",
-            profit: "25",
-            profitPoints: "250",
-            value: "10",
-        },
-        {
-            img: "",
-            title: "Taplandia",
-            profit: "75",
-            profitPoints: "750",
-            value: "25",
-        },
-        {
-            img: "",
-            title: "Memelandia",
-            profit: "150",
-            profitPoints: "1500",
-            value: "50",
-        },
-        {
-            img: "",
-            title: "Bountylandia",
-            profit: "190",
-            profitPoints: "1900",
-            value: "100",
-        },
-        {
-            img: "",
-            title: "Retrodroplandia",
-            profit: "475",
-            profitPoints: "4750",
-            value: "250",
-        },
-        {
-            img: "",
-            title: "Whitelistlandia",
-            profit: "760",
-            profitPoints: "7600",
-            value: "400",
-        },
-        {
-            img: "",
-            title: "Hamsterdam",
-            profit: "950",
-            profitPoints: "9500",
-            value: "500",
-        },
-    ];
-    const degensData = [
-        {
-            img: "",
-            title: "GPU Mining",
-            profit: "1,575",
-            profitPoints: "15,750",
-            value: "750",
-        },
-        {
-            img: "",
-            title: "ASIC Miner",
-            profit: "2,100",
-            profitPoints: "21,000",
-            value: "1,000",
-        },
-        {
-            img: "",
-            title: "ICO Investor",
-            profit: "3,150",
-            profitPoints: "31,500",
-            value: "1,500",
-        },
-        {
-            img: "",
-            title: "IDO Investor",
-            profit: "4,200",
-            profitPoints: "42,000",
-            value: "2,000",
-        },
-        {
-            img: "",
-            title: "Shitcoin Shilling",
-            profit: "5,250",
-            profitPoints: "52,500",
-            value: "2,500",
-        },
-        {
-            img: "",
-            title: "PoS Mining",
-            profit: "6,300",
-            profitPoints: "63,500",
-            value: "3000",
-        },
-        {
-            img: "",
-            title: "Liquid Staking",
-            profit: "7,350",
-            profitPoints: "73,500",
-            value: "3,500",
-        },
-        {
-            img: "",
-            title: "Seed Funding",
-            profit: "8,400",
-            profitPoints: "84,000",
-            value: "4,000",
-        },
-    ];
-    const holdersData = [
-        {
-            img: "",
-            title: "DEX Swap",
-            profit: "11,750",
-            profitPoints: "117,500",
-            value: "5,000",
-        },
-        {
-            img: "",
-            title: "Liquidity Provider",
-            profit: "17,625",
-            profitPoints: "176,250",
-            value: "7,500",
-        },
-        {
-            img: "",
-            title: "DeFi Trader",
-            profit: "23,500",
-            profitPoints: "235,000",
-            value: "10,000",
-        },
-        {
-            img: "",
-            title: "Arbitrage Bot",
-            profit: "29,375",
-            profitPoints: "293,750",
-            value: "12,500",
-        },
-        {
-            img: "",
-            title: "Bridge Asset",
-            profit: "35,250",
-            profitPoints: "352,500",
-            value: "15,000",
-        },
-        {
-            img: "",
-            title: "Cold Wallet",
-            profit: "41,125",
-            profitPoints: "411,250",
-            value: "17,500",
-        },
-        {
-            img: "",
-            title: "GameFi",
-            profit: "47,000",
-            profitPoints: "470,000",
-            value: "20,000",
-        },
-        {
-            img: "",
-            title: "P2P Trading",
-            profit: "52,875",
-            profitPoints: "528,750",
-            value: "22,500",
-        },
-    ];
-    const whalesData = [
-        {
-            img: "",
-            title: "51% Attack",
-            profit: "87,000",
-            profitPoints: "117,500",
-            value: "30,000",
-        },
-        {
-            img: "",
-            title: "Double Spending",
-            profit: "101,500",
-            profitPoints: "1,015,000",
-            value: "35,000",
-        },
-        {
-            img: "",
-            title: "Mt.Gox Hack",
-            profit: "116,000",
-            profitPoints: "1,160,000",
-            value: "40,000",
-        },
-        {
-            img: "",
-            title: "Silk Road",
-            profit: "130,500",
-            profitPoints: "1,305,000",
-            value: "45,000",
-        },
-        {
-            img: "",
-            title: "Coin Mixer",
-            profit: "145,000",
-            profitPoints: "1,450,000",
-            value: "50,000",
-        },
-    ];
 
     return (
         <div className="pb-[100px]">
@@ -241,15 +23,18 @@ export default function UpgradeProfit() {
                     </div>
                 ))}
             </div>
-            <div>
-                <div className=" flex flex-col gap-1 mt-4">
-                    {(tabActive === "Flippers"
+            <div className="flex justify-center items-center  h-full">
+                <h1 className="text-white text-[50px] shadow-lg text-center my-10"
+                    style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}>
+                    Coming Soon
+                </h1>                {/* <div className=" flex flex-col gap-1 mt-4">
+                    {(tabActive === "PR&Team"
                         ? flippersData
-                        : tabActive === "Degens"
+                        : tabActive === "Markets"
                             ? degensData
-                            : tabActive === "Holders"
-                                ? holdersData
-                                : whalesData
+                            : tabActive === "Legal"
+                                ? holdersData : tabActive === "Legal" ? Speclals
+                                    : whalesData
                     ).map((item, idx) => (
                         <div
                             key={idx}
@@ -282,8 +67,9 @@ export default function UpgradeProfit() {
                             </div>
                         </div>
                     ))}
-                </div>
-                <BottomModal isOpen={isOpen} setOpen={setOpen}>
+                </div> */}
+
+                {/* <BottomModal isOpen={isOpen} setOpen={setOpen}>
                     <div className="bg-[#259F7B] pt-[5px] rounded-t-[36px]">
                         <div className="bg-white rounded-t-[36px] pt-1 px-2 flex flex-col gap-4 pb-4">
                             <div className="px-2">
@@ -354,7 +140,7 @@ export default function UpgradeProfit() {
                             </div>
                         </div>
                     </div>
-                </BottomModal>
+                </BottomModal> */}
             </div>
         </div>
     );
