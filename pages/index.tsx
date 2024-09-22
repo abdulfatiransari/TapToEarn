742dec89fe0a1c66cb42352ffb9c2b08177d28d8
HamzaaliUddin <hamzaalikhan20611@gmail.com>
2024-09-22 23:17:50 +0500

    comment score

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,8 +23,6 @@ export default function Home() {
             className="min-h-screen"
             style={{
                 background: "radial-gradient(circle, rgba(90, 0, 150, 0.8) 40%, rgba(0, 0, 0, 1) 100%)",
-                // height: '99vh',
-
             }}>
             {active === "Home" ? (
                 <LandingPage />
